20953afe5702502377a5951d9f9f9c89df7fbce7
Vivien <bjohannavivien@gmail.com>
2025-03-29 12:35:28 +0200

    Eredeti szemi kod, hazi elkezdese

--- Szeminarium1/Program.cs
+++ Szeminarium1/Program.cs
@@ -1,31 +1,45 @@
-﻿using Silk.NET.OpenGL;
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using Szeminarium;
 
-namespace Szeminarium1
+namespace GrafikaSzeminarium
 {
-    internal static class Program
+    internal class Program
     {
         private static IWindow graphicWindow;
 
         private static GL Gl;
 
-        private static uint program;
+        private static ModelObjectDescriptor cube;
+
+        private static CameraDescriptor camera = new CameraDescriptor();
+
+        private static CubeArrangementModel cubeArrangementModel = new CubeArrangementModel();
+
+        private const string ModelMatrixVariableName = "uModel";
+        private const string ViewMatrixVariableName = "uView";
+        private const string ProjectionMatrixVariableName = "uProjection";
 
         private static readonly string VertexShaderSource = @"
         #version 330 core
         layout (location = 0) in vec3 vPos;
 		layout (location = 1) in vec4 vCol;
 
+        uniform mat4 uModel;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+
 		out vec4 outCol;
         
         void main()
         {
 			outCol = vCol;
-            gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0); 
+            gl_Position = uProjection*uView*uModel*vec4(vPos.x, vPos.y, vPos.z, 1.0);
         }
         ";
-        //hianyzok egy vesszo
-        // Vertex shader failed to compile: ERROR: 0:11: '1.0' : syntax error syntax error
+
 
         private static readonly string FragmentShaderSource = @"
         #version 330 core
@@ -38,13 +52,13 @@ namespace Szeminarium1
             FragColor = outCol;
         }
         ";
-        //rossz GLSL verzio van megadva ami nem letezik ezert nem fog lefordulni
-        //Error linking shader Attached fragment shader is not compiled.
+
+        private static uint program;
 
         static void Main(string[] args)
         {
             WindowOptions windowOptions = WindowOptions.Default;
-            windowOptions.Title = "1. szeminárium - háromszög";
+            windowOptions.Title = "Grafika szeminárium";
             windowOptions.Size = new Silk.NET.Maths.Vector2D<int>(500, 500);
 
             graphicWindow = Window.Create(windowOptions);
@@ -52,182 +66,165 @@ namespace Szeminarium1
             graphicWindow.Load += GraphicWindow_Load;
             graphicWindow.Update += GraphicWindow_Update;
             graphicWindow.Render += GraphicWindow_Render;
+            graphicWindow.Closing += GraphicWindow_Closing;
 
             graphicWindow.Run();
         }
 
+        private static void GraphicWindow_Closing()
+        {
+            cube.Dispose();
+            Gl.DeleteProgram(program);
+        }
+
         private static void GraphicWindow_Load()
         {
-            // egszeri beallitasokat
-            //Console.WriteLine("Loaded");
-
             Gl = graphicWindow.CreateOpenGL();
 
+            var inputContext = graphicWindow.CreateInput();
+            foreach (var keyboard in inputContext.Keyboards)
+            {
+                keyboard.KeyDown += Keyboard_KeyDown;
+            }
+
+            cube = ModelObjectDescriptor.CreateCube(Gl);
+
             Gl.ClearColor(System.Drawing.Color.White);
+            
+            Gl.Enable(EnableCap.CullFace);
+            Gl.CullFace(TriangleFace.Back);
+
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.DepthFunc(DepthFunction.Lequal);
+
 
             uint vshader = Gl.CreateShader(ShaderType.VertexShader);
             uint fshader = Gl.CreateShader(ShaderType.FragmentShader);
 
             Gl.ShaderSource(vshader, VertexShaderSource);
             Gl.CompileShader(vshader);
-           // Gl.CompileShader(fshader);
             Gl.GetShader(vshader, ShaderParameterName.CompileStatus, out int vStatus);
             if (vStatus != (int)GLEnum.True)
                 throw new Exception("Vertex shader failed to compile: " + Gl.GetShaderInfoLog(vshader));
 
-
             Gl.ShaderSource(fshader, FragmentShaderSource);
             Gl.CompileShader(fshader);
-            //Gl.CompileShader(vshader);
-            //ha kicserelem a ket compilet akkor az expection lesz kiirva mert nem lesz compilalva a vshader
+            Gl.GetShader(fshader, ShaderParameterName.CompileStatus, out int fStatus);
+            if (fStatus != (int)GLEnum.True)
+                throw new Exception("Fragment shader failed to compile: " + Gl.GetShaderInfoLog(fshader));
 
             program = Gl.CreateProgram();
-            //Gl.AttachShader(program, fshader); //semmilyen hiba nem lesz
-            //Gl.LinkProgram(program); //Error linking shader Link called without any attached shader objects.
             Gl.AttachShader(program, vshader);
-            //Gl.LinkProgram(program); //Fekete lesz a szin mert az szin nem lesz hozzaadva
             Gl.AttachShader(program, fshader);
             Gl.LinkProgram(program);
+
             Gl.DetachShader(program, vshader);
             Gl.DetachShader(program, fshader);
             Gl.DeleteShader(vshader);
             Gl.DeleteShader(fshader);
+            if ((ErrorCode)Gl.GetError() != ErrorCode.NoError)
+            {
+
+            }
 
             Gl.GetProgram(program, GLEnum.LinkStatus, out var status);
             if (status == 0)
             {
                 Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(program)}");
             }
+        }
 
+        private static void Keyboard_KeyDown(IKeyboard keyboard, Key key, int arg3)
+        {
+            switch (key)
+            {
+                case Key.Left:
+                    camera.DecreaseZYAngle();
+                    break;
+                case Key.Right:
+                    camera.IncreaseZYAngle();
+                    break;
+                case Key.Down:
+                    camera.IncreaseDistance();
+                    break;
+                case Key.Up:
+                    camera.DecreaseDistance();
+                    break;
+                case Key.U:
+                    camera.IncreaseZXAngle();
+                    break;
+                case Key.D:
+                    camera.DecreaseZXAngle();
+                    break;
+                case Key.Space:
+                    cubeArrangementModel.AnimationEnabled = !cubeArrangementModel.AnimationEnabled;
+                    break;
+            }
         }
 
         private static void GraphicWindow_Update(double deltaTime)
         {
-            // NO GL
-            // make it threadsave
-            //Console.WriteLine($"Update after {deltaTime} [s]");
+            // NO OpenGL
+            // make it threadsafe
+            cubeArrangementModel.AdvanceTime(deltaTime);
         }
 
         private static unsafe void GraphicWindow_Render(double deltaTime)
         {
-            //Console.WriteLine($"Render after {deltaTime} [s]");
-
             Gl.Clear(ClearBufferMask.ColorBufferBit);
-
-            uint vao = Gl.GenVertexArray();
-            Gl.BindVertexArray(vao);
-
-            float[] vertexArray = new float[] {
-                0f, 0.3f, 0f, //0
-                0.6f, +0.6f, 0f,//1
-                0f, -0.35f, 0f,//2
-                0.45f, 0f, 0f,//3
-
-                -0.6f, +0.6f, 0f,//4
-                0f, 0.3f, 0f, //5
-                -0.45f, 0f, 0f,//6
-                0f, -0.35f, 0f,//7
-
-                -0.6f, +0.6f, 0f,//8
-                0f, 0.3f, 0f, //9
-                0f, 0.9f, 0f, //10
-                0.6f, +0.6f, 0f,//11
-
-                
-
-            };
-
-            float[] colorArray = new float[] {
-                1.0f, 0.0f, 0.0f, 1.0f, //piros //kikommentezve
-                1.0f, 0.0f, 0.0f, 1.0f,
-                1.0f, 0.0f, 0.0f, 1.0f,
-                1.0f, 0.0f, 0.0f, 1.0f,
-
-                0.0f, 0.0f, 1.0f, 1.0f, //kek
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-
-                0.0f, 1.0f, 0.0f, 1.0f, //zold 
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-            };
-
-            uint[] indexArray = new uint[] { 
-                //0, 1, 3, //felcsereles 
-                //0, 2, 3
-                0, 1, 2,
-                2, 1, 3,
-                4,5,6,
-                6,5,7,
-                8,9,10,
-                10,9,11
-            };
-
-            uint vertices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices); //komment
-
-            /*
-            var error = Gl.GetError();
-            if (error != GLEnum.NoError)
-            {
-                Console.WriteLine($"{error}");
-                //hiba lesz mert nem lesz kijelolve hogy melyik bufferrel akarunk majd dolgozni es nem leszs ahova betoltse az adatokat
-            }*/
-
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.EnableVertexAttribArray(0); //0
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(0); //0
-            //ha mind a kettot kiveszem fekete lesz a 3-szog ha csak egyet akkor okes
-
-            /*var error = Gl.GetError();
-            if (error != GLEnum.NoError)
-            {
-                Console.WriteLine($"{error}");
-                //hiba mert a shaderben nincs megfelelo layout(location=2) ezert nem talalja azt
-            }*/
-
-            uint colors = Gl.GenBuffer();
-
-            //Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
-            /*var error = Gl.GetError();
-            if (error != GLEnum.NoError)
-            {
-                Console.WriteLine($"{error}");
-                //hiba lesz mert az adatokat tovabbra is a vertices bufferbe tolti be nem a colorsba 
-            }*/
-            Gl.BindBuffer(GLEnum.ArrayBuffer, colors); //sor csere
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1); //1
-            //ha mind a kettot kiveszem fekete lesz a 3-szog ha csak egyet akkor okes 
-
-            /*var error1 = Gl.GetError();
-            if (error1 != GLEnum.NoError)
-            {
-                Console.WriteLine($"{error1}");
-                //hiba mert a shaderben nincs megfelelo layout(location=3) ezert nem talalja azt
-            }*/
-
-            uint indices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
-            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
-
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+            Gl.Clear(ClearBufferMask.DepthBufferBit);
 
             Gl.UseProgram(program);
-            
-            Gl.DrawElements(GLEnum.Triangles, (uint)indexArray.Length, GLEnum.UnsignedInt, null); // we used element buffer
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
-            Gl.BindVertexArray(vao);
 
-            // always unbound the vertex buffer first, so no halfway results are displayed by accident
-            Gl.DeleteBuffer(vertices);
-            Gl.DeleteBuffer(colors);
-            Gl.DeleteBuffer(indices);
-            Gl.DeleteVertexArray(vao);
+            var viewMatrix = Matrix4X4.CreateLookAt(camera.Position, camera.Target, camera.UpVector);
+            SetMatrix(viewMatrix, ViewMatrixVariableName);
+
+            var projectionMatrix = Matrix4X4.CreatePerspectiveFieldOfView<float>((float)(Math.PI / 2), 1024f / 768f, 0.1f, 100f);
+            SetMatrix(projectionMatrix, ProjectionMatrixVariableName);
+
+
+            var modelMatrixCenterCube = Matrix4X4.CreateScale((float)cubeArrangementModel.CenterCubeScale);
+            SetMatrix(modelMatrixCenterCube, ModelMatrixVariableName);
+            DrawModelObject(cube);
+
+            Matrix4X4<float> diamondScale = Matrix4X4.CreateScale(0.25f);
+            Matrix4X4<float> rotx = Matrix4X4.CreateRotationX((float)Math.PI / 4f);
+            Matrix4X4<float> rotz = Matrix4X4.CreateRotationZ((float)Math.PI / 4f);
+            Matrix4X4<float> roty = Matrix4X4.CreateRotationY((float)cubeArrangementModel.DiamondCubeLocalAngle);
+            Matrix4X4<float> trans = Matrix4X4.CreateTranslation(1f, 1f, 0f);
+            Matrix4X4<float> rotGlobalY = Matrix4X4.CreateRotationY((float)cubeArrangementModel.DiamondCubeGlobalYAngle);
+            Matrix4X4<float> dimondCubeModelMatrix = diamondScale * rotx * rotz * roty * trans * rotGlobalY;
+            SetMatrix(dimondCubeModelMatrix, ModelMatrixVariableName);
+            DrawModelObject(cube);
+
+        }
+
+        private static unsafe void DrawModelObject(ModelObjectDescriptor modelObject)
+        {
+            Gl.BindVertexArray(modelObject.Vao);
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, modelObject.Indices);
+            Gl.DrawElements(PrimitiveType.Triangles, modelObject.IndexArrayLength, DrawElementsType.UnsignedInt, null);
+            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+            Gl.BindVertexArray(0);
+        }
+
+        private static unsafe void SetMatrix(Matrix4X4<float> mx, string uniformName)
+        {
+            int location = Gl.GetUniformLocation(program, uniformName);
+            if (location == -1)
+            {
+                throw new Exception($"{ViewMatrixVariableName} uniform not found on shader.");
+            }
+
+            Gl.UniformMatrix4(location, 1, false, (float*)&mx);
+            CheckError();
+        }
+
+        public static void CheckError()
+        {
+            var error = (ErrorCode)Gl.GetError();
+            if (error != ErrorCode.NoError)
+                throw new Exception("GL.GetError() returned " + error.ToString());
         }
     }
 }
